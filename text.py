b76297a2b289df5e75325112b12a7153a964e070
jovenpey <160571908+jovenpey@users.noreply.github.com>
2024-03-14 11:34:14 +0800

    Update text.py

--- text.py
+++ text.py
@@ -1,1 +1,5 @@
+in_file = /nfs/site/disks/crt_stdcells_001/stdcells/esd_lib/esd783_r0.8.0_23ww28.5/lef/common/ip78_esd_lib.lef
+in_lib = ip78_esd_lib
+in_oas_file= /nfs/site/disks/x5e2d_gwa_jovenpey_01/p1278/work/p3/pdk783_r0.8HP/hdk/pds/oasis/
+
 x = 3
