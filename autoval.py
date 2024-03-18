1d785f40cbbc12b86dc95133644d83879fdcf8a0
jovenpey <160571908+jovenpey@users.noreply.github.com>
2024-03-18 17:22:38 +0800

    Update autoval.py

--- autoval.py
+++ autoval.py
@@ -3,23 +3,23 @@ import glob
 import os.path 
 import re 
 
-print(in_file)
-print(in_lib)
-print(in_oas_file)
+# print(in_file)
+# print(in_lib)
+# print(in_oas_file)
 
-# oas_file = glob.glob(in_oas_file) 
-# file_one = open(in_file, "r") 
-# for line in file_one:
-#     if re.search('MACRO', line): 
+oas_file = glob.glob(in_oas_file) 
+file_one = open(in_file, "r") 
+for line in file_one:
+    if re.search('MACRO', line): 
         
-#         cell = line.replace('MACRO','') 
-#         cell = cell.strip() 
+        cell = line.replace('MACRO','') 
+        cell = cell.strip() 
        
-#         cmd_oas_file = "$SMAKE_LINE" + " " + "lib=" + in_lib + " " + "cell=" + cell + " stream stream_add_options=" + "\"-propMap /p/hdk/pu_tu/prd/gen_data/22.07.18_rc/common/streamout_inst_name_prop.map -replaceBusBitChar\" "
-#         os.system(cmd_oas_file)
+        cmd_oas_file = "$SMAKE_LINE" + " " + "lib=" + in_lib + " " + "cell=" + cell + " stream stream_add_options=" + "\"-propMap /p/hdk/pu_tu/prd/gen_data/22.07.18_rc/common/streamout_inst_name_prop.map -replaceBusBitChar\" "
+        os.system(cmd_oas_file)
        
-#         cmd_oas_file = "smartlauncher --cores 4 --memory 64 rapidesd -layout" + " " + in_oas_file + cell + '.oas' + " " + "-design" + " " + cell + " " + "-pst_pkg 1 -format oas -savedb -work $PWD"   
-#         os.system(cmd_oas_file)
+        cmd_oas_file = "smartlauncher --cores 4 --memory 64 rapidesd -layout" + " " + in_oas_file + cell + '.oas' + " " + "-design" + " " + cell + " " + "-pst_pkg 1 -format oas -savedb -work $PWD"   
+        os.system(cmd_oas_file)
        
-#         print(cmd_oas_file)    
+        print(cmd_oas_file)    
         
