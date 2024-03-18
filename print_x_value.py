8ed5633dda3af6264e58150b64cc819a19c79f7f
jovenpey <160571908+jovenpey@users.noreply.github.com>
2024-03-18 16:48:31 +0800

    Update print_x_value.py

--- print_x_value.py
+++ print_x_value.py
@@ -1,4 +1,8 @@
-from text import x
+from text import in_file, in_lib, in_oas_file
+# from text import x
 
 # Print the value of x
-print("Value of x:", x)
+# print("Value of x:", x)
+print("in_file:", in_file)
+print("in_lib:", in_lib)
+print("in_oas_file:", in_oas_file)
