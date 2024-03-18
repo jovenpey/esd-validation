c27fa4798bf49cc22adfc5ce1faa2880ce5e4297
jovenpey <160571908+jovenpey@users.noreply.github.com>
2024-03-18 17:03:56 +0800

    Update Automation.py

--- Automation.py
+++ Automation.py
@@ -3,9 +3,8 @@ import os.path
 import re 
 import subprocess
 
-in_file = /nfs/site/disks/crt_stdcells_001/stdcells/esd_lib/esd783_r0.8.0_23ww28.5/lef/common/ip78_esd_lib.lef
-in_lib = ip78_esd_lib
-in_oas_file= /nfs/site/disks/x5e2d_gwa_jovenpey_01/p1278/work/p3/pdk783_r0.8HP/hdk/pds/oasis/
+from text import in_file, in_lib, in_oas_file
+
 oas_file = glob.glob(in_oas_file) 
 file_one = open(in_file, "r") 
 for line in file_one:
@@ -22,5 +21,3 @@ for line in file_one:
        
         print(cmd_oas_file)    
         
-
-
